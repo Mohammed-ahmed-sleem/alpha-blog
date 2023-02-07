053f5fa5c7e2ed385c73261494b83de2d2ad1fcd
aliahmed1881 <alisleem2951123@gmail.com>
2023-02-07 10:18:34 +0200

    add create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,28 @@
 class ArticlesController < ApplicationController
     def show
-       # byebug
-        @article = Article.find(params[:id])
+    # byebug
+     @article = Article.find(params[:id])
     end
     def index
-        @articles = Article.all
+     @articles = Article.all
     end 
-end
+    def new
+    end
+    def create
+    @article = Article.new(params.require(:article).permit(:title, :description))  
+    @article.save
+    redirect_to article_path(@article) 
+    # redirect_to @article
+    
+
+
+
+# => require the top level key off article and permit  title and description 
+      #only the permitted attributes are passed into the model
+      #What is params require in Rails?
+      #In Rails, strong params provide an interface for protecting attributes from the end-user assignment.
+      # We can specify required attributes and neglect unnecessary attributes to be used in the Active model mass assignment
+      # (render plain: @article.inspect)
+    end
+
+  end
