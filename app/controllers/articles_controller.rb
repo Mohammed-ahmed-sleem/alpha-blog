23d716b6897adddcd04711f721a32e323e9c89f6
aliahmed1881 <alisleem2951123@gmail.com>
2023-02-13 22:29:47 +0200

    DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,25 @@
 class ArticlesController < ApplicationController
-    def test
-    end
-    def destroy
-      @article = Article.find(params[:id])
-      @article.destroy
-      flash[:succes] = "Article was  destroyed"
-       redirect_to articles_path
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+   
+  def test
     end
 
     def show
     # byebug
-     @article = Article.find(params[:id])
     end
     def index
       @articles = Article.all
     end 
+    
     def new
       @article =Article.new # initiat an @article instance variable for (@article.errors.any?) 
     end
 
     def edit
-      @article = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
           flash[:notice] = "Article was created successfully."
           redirect_to @article
@@ -34,14 +29,29 @@ class ArticlesController < ApplicationController
     end
 
     def update
-      @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
+      if @article.update(article_params)
         flash[:notice] = "Article was updated successfully"
         redirect_to @article
       else
         render 'edit'
       end
     end
+
+    def destroy
+      @article.destroy
+      flash[:succes] = "Article was  destroyed"
+       redirect_to articles_path
+    end
+
+    private 
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
 
 
   
